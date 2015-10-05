9444361a80390a07d12d3e6ab4aca7508a1e31af
_ <tsuhaso@gmail.com>
2015-10-05 15:33:54 +0900

    a

--- get_last_result.m
+++ get_last_result.m
@@ -1,64 +1,17 @@
 function [data] = get_last_result (name)
 
-fp = fopen([name '/output.csv']);
+d = csvread([name '/output.csv']);
+s = size(d);
+data(s(1), (s(2) / 3 / 2)) = intval(0.0);
 
-if fp ~= -1
-    count = 1;
+data(:, 1) = infsup( ...
+    (-1) .^ d(:, 1) .* 2 .^ d(:, 2) .* d(:, 3), ...
+    (-1) .^ d(:, 4) .* 2 .^ d(:, 5) .* d(:, 6));
 
-    linec = 1;
-    line = fgetl(fp);
-
-    col = fix((length(strfind(line, ',')) + 1) / 2);
-
-    if mod(length(strfind(line, ',')) + 1, 2) == 1
-        error([name '/output.csv is invalid']);
-    end
-
-    while true
-        line = fgetl(fp);
-        if line == -1
-            break;
-        end
-        linec = linec + 1;
-    end
-
-    data(linec, col) = infsup(0, 0);
-
-    fseek(fp, 0, 'bof');
-
-    while true
-        line = fgetl(fp);
-
-        if line == -1
-            break;
-        end
-
-        indices = [strfind(line, ',') length(line)+1];
-
-        if length(indices) ~= col * 2
-            break;
-        end
-
-        b = 1;
-
-        for i = 0 : length(indices) / 2 - 1
-            e = indices(2 * i + 1);
-            inf = line(b : e - 1);            
-            b = e + 1;
-            e = indices(2 * i + 2);
-            sup = line(b : e - 1);
-            
-            data(count, i + 1) = hull(intval(inf), intval(sup));
-
-            b = e + 1;
-        end
-
-        count = count + 1;
-    end
-
-    fclose(fp);
-else
-    error(['file ' name '/output.csv was not found']);
+for i = 1 : (s(2) / 3 / 2) - 1
+    data(:, i + 1) = infsup( ...
+        (-1) .^ d(:, i * 6 + 1) .* 2 .^ d(:, i * 6 + 2) .* d(:, i * 6 + 3), ...
+        (-1) .^ d(:, i * 6 + 4) .* 2 .^ d(:, i * 6 + 5) .* d(:, i * 6 + 6));
 end
 
 end
